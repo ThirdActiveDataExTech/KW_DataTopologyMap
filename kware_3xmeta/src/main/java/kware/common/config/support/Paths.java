af5259d2e81b85546227d8d924a8115b3d585095
kware-dev01 <kware-dev01@kware.co.kr>
2025-05-20 17:20:51 +0900

    차트 작업부분 일부반영

--- kware_3xmeta/src/main/java/kware/common/config/support/Paths.java
+++ kware_3xmeta/src/main/java/kware/common/config/support/Paths.java
@@ -4,7 +4,16 @@ public class Paths {
 
 
     public static String[] EXCLUDE_URL = {
-            "/login", "/assets/**", "/sessionExpired", "/login/force.json"
+            "/login",
+            "/assets/**",
+            "/sessionExpired",
+            "/login/force.json",
+            "/asp/signup",
+            "/cetus/api/user/checkEmail.json",
+            "/cetus/api/user/checkId.json",
+            "/cetus/api/user",
+            "/invite/validate",
+            "/asp/expired"
             ,"/asp/signup"
     };
 }
