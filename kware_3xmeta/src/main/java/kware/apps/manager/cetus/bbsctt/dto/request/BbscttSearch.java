fbc67b46d61acaed4892d5326bc2526b116ea7c8
kware-dev01 <kware-dev01@kware.co.kr>
2025-06-10 09:20:43 +0900

    20250610 작업부분 반영
    - 게시글 목록 > 공지게시판 : 공지Y 상단 위치
    - sidebar-download index 초기화

--- kware_3xmeta/src/main/java/kware/apps/manager/cetus/bbsctt/dto/request/BbscttSearch.java
+++ kware_3xmeta/src/main/java/kware/apps/manager/cetus/bbsctt/dto/request/BbscttSearch.java
@@ -9,10 +9,12 @@ public class BbscttSearch {
     private Long bbsUid;
     private Long regUid;
     private String openAt;
+    private String orderByNotice;
 
-    public BbscttSearch(Long bbsUid, Long regUid, String openAt) {
+    public BbscttSearch(Long bbsUid, Long regUid, String openAt, String orderByNotice) {
         this.bbsUid = bbsUid;
         this.regUid = regUid;
         this.openAt = openAt;
+        this.orderByNotice = orderByNotice;
     }
 }
