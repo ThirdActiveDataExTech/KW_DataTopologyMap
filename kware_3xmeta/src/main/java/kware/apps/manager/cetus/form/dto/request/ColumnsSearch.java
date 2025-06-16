722ed103c0bcffd6559dc94a8734ed82c2d05621
kware-dev01 <kware-dev01@kware.co.kr>
2025-06-16 16:49:40 +0900

    workplaceUid 1l -> getWorkplaceUid

--- kware_3xmeta/src/main/java/kware/apps/manager/cetus/form/dto/request/ColumnsSearch.java
+++ kware_3xmeta/src/main/java/kware/apps/manager/cetus/form/dto/request/ColumnsSearch.java
@@ -11,6 +11,6 @@ public class ColumnsSearch {
      public ColumnsSearch(String type, String formGroup) {
          this.type = type;
          this.formGroup = formGroup;
-         this.workplaceUid = 1l;
+         this.workplaceUid = UserUtil.getUserWorkplaceUid();
      }
  }
