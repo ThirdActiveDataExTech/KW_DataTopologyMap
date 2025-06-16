547150edacb3a43b9c6ff7865efa7836eda1e3c6
kware-dev01 <kware-dev01@kware.co.kr>
2025-06-16 17:47:24 +0900

    20250616 오후 작업부분 반영
    - 홈화면 및 리스트 화면
    - 관련기관 로고이미지 변경

--- kware_3xmeta/src/main/java/kware/apps/manager/cetus/contents/categories/Sources.java
+++ kware_3xmeta/src/main/java/kware/apps/manager/cetus/contents/categories/Sources.java
@@ -10,7 +10,8 @@ public class Sources {
 
     public static final List<EnumCodeDto> ITEMS = List.of(
         new EnumCodeDto("sbsNews", "SBS 뉴스팀"),
-        new EnumCodeDto("sbsDrama", "SBS 드라마 제작국"),
+        new EnumCodeDto("sbsDrama", "SBS 드라마팀"),
+        new EnumCodeDto("sbsEnter", "SBS 예능팀"),
         new EnumCodeDto("sbsSports", "SBS 스포츠국"),
         new EnumCodeDto("external", "외부 제작 콘텐츠")
     );
